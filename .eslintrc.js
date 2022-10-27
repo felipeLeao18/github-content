f34b50ed06540cdc997b4eb6ad796069bfd67301
Felipe Leao <flpleao1807@gmail.com>
2022-10-27 17:53:29 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'no-console': 'warn'
+  }
+}
